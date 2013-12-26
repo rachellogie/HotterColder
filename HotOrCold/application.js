ff3a8593777f2a32f5b6432e5ad57ca8216101ee
rachellogie <rachel.logi@gmail.com>
2013-12-26 16:12:43 -0700

    bug fix
    bug fix for multiple spaces, reseting the page, and stop user from
    guessing after already won

--- HotOrCold/application.js
+++ HotOrCold/application.js
@@ -4,41 +4,48 @@ $(document).ready(function(){
 	var guess;
 	var numGuesses = 0;
 	var previousDifference;
+	var keepGoing = true;
 	
 	$(".inputform").submit(function(event){
 			event.preventDefault();
 			guess = document.getElementById("yourGuess").value;	
 			var difference = Math.abs(actual - guess);
 			
-			console.log("your guess is " + guess + "actual is " + actual);
+			console.log("your guess is " + guess + " actual is " + actual);
 			
-			if(!inRange(guess)){
-				console.log("You must pick a number from 1 to 100");
-				setMessage("You must pick a number from 1 to 100!!! Do it right!");
-				reset();
-			} else if(isCorrect(guess)){
-				console.log("You got it!!");
-				setMessage("You got it!  Click the button below to start a new game.");
-				reset();
-			} else {
-				console.log("you are" + check(difference));
-				var temperature = check(difference);
-				if (numGuesses == 0){
-					setMessage("You are " + temperature + ".");
-					previousDifference = difference;
-					numGuesses++;
+			if(keepGoing){
+				if(!inRange(guess)){
+					console.log("You must pick a number from 1 to 100");
+					setMessage("You must pick a number from 1 to 100!!! Do it right!");
+					reset();
+				} else if(isCorrect(guess)){
+					console.log("You got it!!");
+					setMessage("You got it!  Click the button below to start a new game.");
+					reset();
+					keepGoing = false;
 				} else {
-					var direction = gettingWarmer(difference, previousDifference);
-					console.log("difference is " + difference);
-					console.log("previousdifference is " + previousDifference);
-					console.log(direction);
-					console.log(difference);
-					setMessage("You are " + temperature + ", and you're getting " + direction + ".");
-					previousDifference = difference;
-				}
+					console.log("you are" + check(difference));
+					var temperature = check(difference);
+					if (numGuesses == 0){
+						setMessage("You are " + temperature + ".");
+						previousDifference = difference;
+						numGuesses++;
+					} else {
+						var direction = gettingWarmer(difference, previousDifference);
+						console.log("difference is " + difference);
+						console.log("previousdifference is " + previousDifference);
+						console.log(direction);
+						console.log(difference);
+						setMessage("You are " + temperature + ", and you're getting " + direction + ".");
+						previousDifference = difference;
+					}
 
-				
-				$("ul").append("<li>" + guess + "</li>")
+					
+					$("ul").append("<li>" + guess + "</li>")
+					reset();
+				}
+			}else {
+				setMessage("You already won this one. Click the button below to start a new game.")
 				reset();
 			}
 	});
@@ -46,7 +53,11 @@ $(document).ready(function(){
 
 
 	$(".clear").click(function(event){
-		/*event.preventDefault();*/
+		event.preventDefault();
+		$("li").remove();
+		numGuesses = 0;
+		keepGoing = true;
+		setMessage("Let the games begin!");
 		actual = Math.floor(Math.random()*101);
 		console.log("actual is now " + actual);
 	});	
@@ -75,7 +86,7 @@ $(document).ready(function(){
 			return false;
 		} else if (isNaN(guess)) {
 			return false;
-		} else if (guess === "" || guess === " "){
+		} else if ($.trim(guess) == ""){
 			return false;
 		} else {
 			return true;
